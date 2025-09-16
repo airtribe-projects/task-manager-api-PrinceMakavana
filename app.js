7ddb4fd3cf72b2be65ca5ea55e1f68cfb66f9f85
PrinceMakavana <princemakavana61@gmail.com>
2025-09-16 20:01:00 +0530

    Add CRUD of task list

--- app.js
+++ app.js
@@ -2,16 +2,77 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let tasks = [
+    {
+        id: 1,
+        title: "Set up environment",
+        description: "Install Node.js, npm, and git",
+        completed: true
+    },
+    {
+        id: 2,
+        title: "Create project structure",
+        description: "Set up folders and files for the project",
+        completed: false
+    }
+];
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
+app.post('/tasks', (req, res) => {
+    const { title, description, completed } = req.body;
+    if (!title || !description || typeof completed !== 'boolean') {
+        res.status(400).send({ error: 'Invalid task data' });
     }
-    console.log(`Server is listening on ${port}`);
+    const newTask = {
+        id: tasks.length + 1,
+        title,
+        description,
+        completed
+    };
+    tasks.push(newTask);
+    res.status(201).json(newTask)
 });
 
+app.get('/tasks' , (req, res) => {
+    res.status(200).send(tasks);
+});
 
+app.get('/tasks/:id', (req , res) => {
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(t => t.id == taskId);
+    if (!task) {
+        res.status(404).send({ error: 'Task not found' });
+    }
+    if (task) {
+        res.status(200).json(task);
+    }
+})
+
+app.put('/tasks/:id', (req , res) => {
+    const taskId = parseInt(req.params.id);
+    const { title, description, completed } = req.body;
+    const taskIndex = tasks.findIndex(t => t.id === taskId);
+    if (taskIndex === -1) {
+         res.status(404).send({ error: 'Task not found' });
+    }
+    if (!title || !description || typeof completed !== 'boolean') {
+        res.status(400).send({ error: 'Invalid task data' });
+    }
+    tasks[taskIndex] = { id: taskId, title, description, completed };
+    res.status(200).json(tasks[taskIndex]);
+})
+
+app.delete('/tasks/:id', (req , res) => {
+    const taskId = parseInt(req.params.id);
+    const taskIndex = tasks.findIndex(t => t.id === taskId);
+    if (taskIndex === -1) {
+         res.status(404).send({ error: 'Task not found' });
+    }
+    tasks.splice(taskIndex, 1);
+    res.status(200).send();
+});
 
 module.exports = app;
